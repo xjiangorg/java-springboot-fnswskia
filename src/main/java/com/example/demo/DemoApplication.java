405b943463ebdfe7f6014774413f8ed7d344fbc5
Xin Jiang <53755997+xinredhat@users.noreply.github.com>
2025-06-04 14:28:44 +0800

    Test commit from TSSC e2e test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hello World - Updated! 1749018516682 !";
     }
 
     public static void main(String[] args) {
